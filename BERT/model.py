1430423cc178fbbbf67697b91bf2d1d329fd942d
Rafael Cano <rafael.cano@ucsp.edu.pe>
2021-12-15 15:36:16 -0500

    Reorganized repo for final presentation.

--- BERT/model.py
+++
@@ -1,4 +0,0 @@
-from transformers import BertTokenizer, TFBertForSequenceClassification
-from transformers import InputExample, InputFeatures
-model = TFBertForSequenceClassification.from_pretrained("bert-base-uncased")
-tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
